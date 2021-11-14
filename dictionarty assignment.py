0bc8921b0215d790219d8598bfd4950a845cf95f
urmid2421 <94037896+urmid2421@users.noreply.github.com>
2021-11-14 10:38:35 +0530

    Add files via upload

---
+++ dictionarty assignment.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+# my_dict = {}
+ 
+for i in range(97, 97 + 26):
+    my_dict[chr(i)] = i
+ 
+print(my_dict)
+
+
+# In[ ]:
+
+
+
+
---
+++ lists and tuples assignment.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def last(n): return n[-1]
+
+def sort_list_last(tuples):
+  return sorted(tuples, key=last)
+
+print(sort_list_last([(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]))
+
+
+# In[ ]:
+
+
+
+
